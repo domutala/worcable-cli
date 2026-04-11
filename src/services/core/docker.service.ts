3513a6400ce3af61c72bfbb164ed260ea1f27e68
Mamadou Talla DIA <ibntalla@gmail.com>
2026-04-11 09:19:38 +0000

    Renommer la variable de nom de conteneur et ajuster la configuration des labels pour Traefik

--- src/services/core/docker.service.ts
+++ src/services/core/docker.service.ts
@@ -1,17 +1,16 @@
-import Dockerode from "dockerode";
 import * as compose from "docker-compose";
 import type { Service } from "docker-compose/dist/compose-spec";
 import type { Config } from "../../types";
 
 export async function runDocker(config: Config) {
   const tag = config.version.replaceAll(".", "").replaceAll("/", "-");
-  const branchName = `worcable-core-${tag}`;
+  const containerName = `worcable-core-${tag}`;
   const domain = config.services.core.appUrl.replace(/^https?:\/\//, "");
   const networName = config.user.dockerNetwork;
 
   const service: Service = {
     image: `domutala/worcable-core:${config.version}`,
-    container_name: branchName,
+    container_name: containerName,
     restart: "always",
     env_file: ".env",
     environment: { NODE_ENV: "production" },
@@ -28,18 +27,24 @@ export async function runDocker(config: Config) {
   const labels: string[] = [];
 
   if (config.user.reverseProxy === "traefik") {
-    service.ports = undefined;
-
     labels.push(
-      "traefik.enable=true",
-      `traefik.http.routers.worcable-${branchName}.rule=Host('${domain}')`,
-      `traefik.http.routers.worcable-${branchName}.entrypoints=web,websecure`,
-      `traefik.http.routers.worcable-${branchName}.tls.certresolver=myresolver`,
-      `traefik.http.services.worcable-${branchName}.loadbalancer.server.port=${80}`,
-      `traefik.http.routers.worcable-${branchName}-http.rule=Host('${domain}')`,
-      `traefik.http.routers.worcable-${branchName}-http.entrypoints=web`,
-      `traefik.http.routers.worcable-${branchName}-http.middlewares=redirect-to-http`,
-      `traefik.http.middlewares.redirect-to-https.redirectscheme.scheme=https`
+      `traefik.enable=true`,
+      `traefik.http.routers.worcable-${containerName}.rule=Host('${domain}')`,
+      `traefik.http.routers.worcable-${containerName}.entrypoints=web,websecure`,
+      `traefik.http.routers.worcable-${containerName}.tls.certresolver=myresolver`,
+      `traefik.http.services.worcable-${containerName}.loadbalancer.server.port=80`,
+      `traefik.http.middlewares.redirect-to-https.redirectscheme.scheme=https`,
+      `traefik.http.routers.worcable-${containerName}.middlewares=redirect-to-https`
+
+      // "traefik.enable=true",
+      // `traefik.http.routers.worcable-${containerName}.rule=Host('${domain}')`,
+      // `traefik.http.routers.worcable-${containerName}.entrypoints=web,websecure`,
+      // `traefik.http.routers.worcable-${containerName}.tls.certresolver=myresolver`,
+      // `traefik.http.services.worcable-${containerName}.loadbalancer.server.port=${80}`,
+      // `traefik.http.routers.worcable-${containerName}-http.rule=Host('${domain}')`,
+      // `traefik.http.routers.worcable-${containerName}-http.entrypoints=web`,
+      // `traefik.http.routers.worcable-${containerName}-http.middlewares=redirect-to-http`,
+      // `traefik.http.middlewares.redirect-to-https.redirectscheme.scheme=https`
     );
   }
 
