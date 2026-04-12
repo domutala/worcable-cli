79a7b135dfcb41de916e4f67f80cda862a5670a6
Mamadou Talla DIA <ibntalla@gmail.com>
2026-04-12 18:16:23 +0000

    Correction de la version dans package.json et amélioration de la gestion des erreurs dans index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,32 @@
+import pkg from '../package.json' with { type: 'json' };
 import { cac } from "cac";
 import { installCommand } from "./commands/install";
 import { reverseProxyCommand } from "./commands/reverse_proxy";
+import { pico } from './utils/pico';
 
 const cli = cac("worcable");
 
-cli.command("install", "Install Worcable").action(installCommand);
-cli.command("reverse-proxy", "Reverse prpoxy").action(reverseProxyCommand);
+cli
+  .command("install", "Install Worcable on the system")
+  .alias("i")
+  .action(installCommand);
 
+cli
+  .command("reverse-proxy", "Start Worcable reverse proxy")
+  .alias("rp")
+  .action(reverseProxyCommand);
+
+  cli.version(pkg.version);
 cli.help();
 cli.parse();
+
+process.on("uncaughtException", (error) => {
+  if (error.name === "ExitPromptError") {
+    pico.clear().render('red', '🗙').text("shut down").log();
+    process.exit(0);
+  }
+
+  console.error(error);
+  process.exit(1);
+});
+
